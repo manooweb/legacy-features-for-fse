a9fe0c167ec9e659d2abc5c44bc635f982419048
Hug0-Drelon <hugo.drelon@gmail.com>
2022-07-29 20:53:38 +0200

    Bring code from x2p0-legacy-widget and add style for menu o n front and admin.

--- legacy-features-fse.php
+++ legacy-features-fse.php
@@ -26,6 +26,10 @@ namespace WP_Syntex\Legacy_Features_for_FSE;
 
 defined( 'ABSPATH' ) || exit;
 
+define( 'LFFF_DIR', __DIR__ );
+
+define( 'LFFF_FILE', __FILE__ );
+
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require_once __DIR__ . '/vendor/autoload.php';
 }
