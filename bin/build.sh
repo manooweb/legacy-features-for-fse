b73ccb1ff25598f192a381827256dec317006210
Emmanuel Hesry <ehesry@gmail.com>
2022-08-03 12:23:54 +0200

    Add composer build script command and the shell script accordingly

---
+++ bin/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Installing PHP packages..."
+composer install
+
+echo "Running Polylang build..."
+npm install && npm run build
+
+echo "Build done!"
+
+echo "Cleanup" # Discard composer.lock and package-lock.json changings to ensure they'll never be pushed on repository.
+git checkout -- composer.lock package-lock.json
