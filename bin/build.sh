4184ddaaf0945547f2bb2a5e714ec24cdfdd0a4d
Emmanuel Hesry <ehesry@gmail.com>
2022-08-04 08:07:52 +0200

    Remove useless cleanup in build.sh script

--- bin/build.sh
+++ bin/build.sh
@@ -6,6 +6,3 @@ echo "Running Polylang build..."
 npm install && npm run build
 
 echo "Build done!"
-
-echo "Cleanup" # Discard composer.lock and package-lock.json changings to ensure they'll never be pushed on repository.
-git checkout -- composer.lock package-lock.json
